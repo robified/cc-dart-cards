9eeb15ae97edfc4221d39c9112d230f9d1653f64
Robin <daorob@gmail.com>
2020-01-13 15:50:23 -0600

    Add fields to classes.

--- deckOfCards.dart
+++ deckOfCards.dart
@@ -1,3 +1,11 @@
-class Deck {}
+// a deck will have a list of card instances
+// List is almost like array
+class Deck {
+  List<Card> cards;
+}
 
-class Card {}
+// each card needs a rank and suit, both will be string type
+class Card {
+  String suit;
+  String rank;
+}
