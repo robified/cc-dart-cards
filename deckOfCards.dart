537b9795fba065dbb4434b4f47c9e3c00b13b307
Robin <daorob@gmail.com>
2020-01-13 16:12:27 -0600

    Add nested for loops.

--- deckOfCards.dart
+++ deckOfCards.dart
@@ -3,7 +3,29 @@
 class Deck {
   List<Card> cards;
 
-  Deck() {}
+  Deck() {
+    var ranks = [
+      'Ace',
+      'Two',
+      'Three',
+      'Four',
+      'Five',
+      'Six',
+      'Seven',
+      'Eight',
+      'Nine',
+      'Ten',
+      'Jack',
+      'Queen',
+      'King',
+      'Joker'
+    ];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {}
+    }
+  }
 }
 
 // each card needs a rank and suit, both will be string type
